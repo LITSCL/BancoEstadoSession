941ffb6abc4eb9eabb77a82b3b5d867c55bdad2b
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-11 16:58:25 -0300

    Ok

--- src/cl/inacap/bancoestadosession/model/Usuario.java
+++ src/cl/inacap/bancoestadosession/model/Usuario.java
@@ -9,15 +9,19 @@ public class Usuario {
 		this.rut = rut;
 		this.clave = clave;
 	}
+
 	public String getRut() {
 		return rut;
 	}
+	
 	public void setRut(String rut) {
 		this.rut = rut;
 	}
+
 	public String getClave() {
 		return clave;
 	}
+	
 	public void setClave(String clave) {
 		this.clave = clave;
 	}
